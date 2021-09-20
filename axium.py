1a33d96ddde9e751b4e5cc7cb98a1afa9ef83bef
Daniel Pope <mauve@mauveweb.co.uk>
2021-09-20 12:18:37 +0100

    Add enemies that shoot

--- axium.py
+++ axium.py
@@ -18,6 +18,8 @@ BULLET_SPEED = 700  # px/s
 scene = w2d.Scene(1280, 720, title="Axium")
 bg = scene.layers[-3].add_sprite('space', pos=(0, 0))
 
+coro = w2d.clock.coro
+
 
 def read_joy() -> vec2:
     jx = stick.get_axis(0)
@@ -34,7 +36,7 @@ async def bullet(ship):
         pos=pos,
         angle=ship.angle,
     )
-    async for dt in w2d.clock.coro.frames_dt(seconds=3):
+    async for dt in coro.frames_dt(seconds=3):
         shot.pos += vel * dt
     shot.delete()
 
@@ -47,17 +49,72 @@ async def joy_press(*buttons):
             return ev
 
 
+def trail(obj, color='white', stroke_width=2):
+    trail = scene.layers[1].add_line(
+        [obj.pos] * 50,
+        color=color,
+        stroke_width=stroke_width,
+    )
+    *_, alpha = trail.color
+    colors = trail.colors
+    colors[:, 3] = np.linspace(alpha, 0, 50) ** 2
+    trail.colors = colors
+
+    try:
+        while True:
+            yield
+            stern = obj.pos + vec2(-10, 0).rotated(obj.angle)
+            trail.vertices = np.vstack([
+                [stern],
+                trail.vertices[:-1]
+            ])
+    finally:
+        trail.delete()
+
+
+async def threx_shoot(ship):
+    sfx.enemy_laser.play()
+    vel = vec2(BULLET_SPEED, 0).rotated(ship.angle) + ship.vel
+    pos = ship.pos + vec2(20, 0).rotated(ship.angle)
+    shot = w2d.Group([
+        scene.layers[1].add_sprite('threx_bullet1'),
+        scene.layers[1].add_sprite('threx_bullet2'),
+    ],
+        pos=pos
+    )
+    async for dt in coro.frames_dt():
+        shot.pos += vel * dt
+        shot[0].angle += 4 * dt
+        shot[1].angle -= 2 * dt
+    shot.delete()
+
+
+async def do_threx(bullet_nursery):
+    """Coroutine to run an enemy ship."""
+    ship = scene.layers[0].add_sprite('threx')
+    ship.vel = vec2(0, 0)
+    t = trail(ship, color='red', stroke_width=1)
+    async def drive():
+        async for dt in coro.frames_dt():
+            ship.x += 30 * dt
+            next(t)
+
+    async def shoot():
+        while True:
+            bullet_nursery.do(threx_shoot(ship))
+            await coro.sleep(1)
+
+    async with w2d.Nursery() as ns:
+        ns.do(drive())
+        ns.do(shoot())
+
+
 async def do_life():
     ship = scene.layers[0].add_sprite('ship')
-    trail = scene.layers[1].add_line(
-        [ship.pos] * 50,
-        color=(0.6, 0.8, 1.0, 0.6),
-        stroke_width=3,
-    )
-
     async def drive_ship():
         vel = ship.vel = vec2(0, 0)
-        async for dt in w2d.clock.coro.frames_dt():
+        t = trail(ship, color=(0.6, 0.8, 1.0, 0.9))
+        async for dt in coro.frames_dt():
             ship.pos += vel * dt
             scene.camera.pos = ship.pos
             bg.pos = ship.pos - 0.03 * scene.camera.pos
@@ -65,18 +122,13 @@ async def do_life():
                 ship.angle = vel.angle()
             vel = vel * (DECEL ** dt) + read_joy() * ACCEL * dt
             ship.vel = vel
-
-            stern = ship.pos + vec2(-10, 0).rotated(ship.angle)
-            trail.vertices = np.vstack([
-                [stern],
-                trail.vertices[:-1]
-            ])
+            next(t)
 
     async def shoot():
         while True:
             await joy_press(0)
             ns.do(bullet(ship))
-            await w2d.clock.coro.sleep(0.1)
+            await coro.sleep(0.1)
 
     async with w2d.Nursery() as ns:
         ns.do(drive_ship())
@@ -85,7 +137,9 @@ async def do_life():
 
 async def main():
     for _ in range(3):
-        await do_life()
+        async with w2d.Nursery() as game:
+            game.do(do_life())
+            game.do(do_threx(game))
 
 
 w2d.run(main())
--- sfx.py
+++ sfx.py
@@ -19,3 +19,16 @@ laser = Sound(buffer=pyfxr.SFX(
     pha_ramp=-0.132,
     wave_type=pyfxr.WaveType.SQUARE,
 ))
+
+enemy_laser = Sound(buffer=pyfxr.SFX(
+    base_freq=0.46,
+    freq_limit=0.2,
+    freq_ramp=-0.187,
+    duty=0.88,
+    duty_ramp=-0.067,
+    env_attack=0.0,
+    env_sustain=0.285,
+    env_decay=0.145,
+    env_punch=0.043,
+    wave_type=pyfxr.WaveType.SQUARE,
+))
